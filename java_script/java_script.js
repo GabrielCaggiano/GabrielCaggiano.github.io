760c2d4cd98fc58441d07c82aa695db0a1adbdc7
pureGabe1021 <gcaggiano@purestorage.com>
2021-05-15 20:16:21 -0600

    lesson4

--- java_script/java_script.js
+++ java_script/java_script.js
@@ -1,2 +1,7 @@
 document.getElementById("assignment-last-modified").innerHTML = document.lastModified;
 document.getElementById("copy-year").innerHTML = new Date().getFullYear();
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+   }
