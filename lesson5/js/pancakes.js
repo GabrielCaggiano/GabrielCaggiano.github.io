e7063b12ed8f76c7e49c6f72668e676d6d2d5536
GabrielCaggiano <kg.caggi1021@gmail.com>
2021-05-29 19:28:22 -0600

    lesson6

---
+++ lesson5/js/pancakes.js
@@ -0,0 +1,5 @@
+var day = (new Date()).getDay();
+
+if (day == 5) {
+    document.getElementById('pancakes').textContent = ("Saturday = Preston Pancakes in the Park!  9:00 a.m. at the city park pavilion.")
+}
