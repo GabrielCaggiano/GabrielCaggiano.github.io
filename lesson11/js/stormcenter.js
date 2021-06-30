6b0b4db1d2e889ad77b5956cf1142011f1143560
GabrielCaggiano <kg.caggi1021@gmail.com>
2021-06-30 01:36:44 -0600

    lessson11

--- lesson11/js/stormcenter.js
+++ lesson11/js/stormcenter.js
@@ -1,7 +1,3 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-function myFunction() {
-    location.replace("https://gabrielcaggiano.github.io/lesson8/thanks.html")
-  }
