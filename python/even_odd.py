dbad18525f9992a2bb7248acecc87a5565371848
pureGabe1021 <gcaggiano@purestorage.com>
2021-09-27 13:02:01 -0600

    python

---
+++ python/even_odd.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python
+
+def even_odd():
+    number = input("Enter a number: ")
+    n = int(number)
+    if  n %  2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+even_odd()
+
